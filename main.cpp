838363b19365065acaed5b810a22b3fcc0871344
brigita <brigita.cironytee@gmail.com>
2018-05-23 21:47:55 +0300

    skaiciuoja eilutes

--- main.cpp
+++ main.cpp
@@ -1,25 +1,31 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <sstream> 
 #include <map>
- #include <string>
+#include <string>
 int main()
 {
+    auto eile=0;
     std::map<std::string, size_t>  word_map;
-    std::string w;
-    for (;std::cin>>w;)  
-        {
-        if (w.back()=='.' || w.back()==',' || w.back()==')') w = w.substr(0, w.size()-1);
-        if (w.front()=='(') w = w.substr(1, w.size());
-         ++word_map[w]; 
-        }
- 
+    std::string eilute;
+    for(;std::getline(std::cin,eilute);)
+    {
+        eile++;
+        std::istringstream eilute2(eilute);
+        std:: string w;
+        for (;eilute2>>w;)  
+            {
+                if (w.back()=='.' || w.back()==',' || w.back()==')') w = w.substr(0, w.size()-1);
+                if (w.front()=='(') w = w.substr(1, w.size());
+                ++word_map[w]; 
+            }
+    }
     for (const auto &pair : word_map) 
     { 
         if (pair.second>2) 
         {
-            std::cout << pair.second;
-            std::cout << " occurrences of word '" << pair.first << "'\n"; 
+            std::cout <<"'"<<pair.first<<"'"<< " žodis pasikartoja " << pair.second <<" kartus ir yra šiose eilutėse: "<<std::endl; 
         }
     }
 }
