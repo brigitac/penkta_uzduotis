ff22beed74a6f907bb2acb105b7f926d4d990190
brigita <brigita.cironytee@gmail.com>
2018-05-24 08:40:30 +0300

    pridetas nurodymas, kuriose eilutese yra zodis

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@ int main()
 {
     auto eile=0;
     std::map<std::string, size_t>  word_map;
+    std::map<std::string, std::string>  line_map;
     std::string eilute;
     for(;std::getline(std::cin,eilute);)
     {
@@ -18,14 +19,17 @@ int main()
             {
                 if (w.back()=='.' || w.back()==',' || w.back()==')') w = w.substr(0, w.size()-1);
                 if (w.front()=='(') w = w.substr(1, w.size());
-                ++word_map[w]; 
+                ++word_map[w];
+                if (word_map[w]>1)  line_map[w]=line_map[w] +", " +std::to_string(eile);
+                else line_map[w]=std::to_string(eile);
+                std::cout<<word_map[w]<<" "<<line_map[w]<<std::endl;
             }
     }
-    for (const auto &pair : word_map) 
-    { 
-        if (pair.second>2) 
+    for(auto it = word_map.cbegin(); it != word_map.cend(); ++it)
+    {
+     if (it->second>1) 
         {
-            std::cout <<"'"<<pair.first<<"'"<< " žodis pasikartoja " << pair.second <<" kartus ir yra šiose eilutėse: "<<std::endl; 
-        }
+         std::cout <<"'"<<it->first<<"'"<< " žodis pasikartoja " << it->second <<" kartus ir yra šiose eilutėse: "<<line_map[it->first]<<std::endl;
+        } 
     }
 }
